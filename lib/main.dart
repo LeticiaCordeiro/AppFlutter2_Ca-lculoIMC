c25ce4f8256246410a408b70b725998ac1d6e7a3
Unknown <leticia.cordeiro98@gmail.com>
2018-11-25 11:08:07 -0200

    47. Formulários e Validações

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,39 @@ class _HomeState extends State<Home> {
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
 
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  
   //var de resposta
   String _infoText = "Informe seus dados!";
   //limpar a tela 
   void _resetFields(){
     weightController.text = "";
     heightController.text = "";
-    _infoText = "Informe seus dados!!";
+    setState(() {
+        _infoText = "Informe seus dados!!";  
+    });
   }
   //calcular 
   void calculate(){
-    double weight = double.parse(weightController.text);
-    double height = double.parse(heightController.text) / 100;
-    double imc = weight/(height * height);
-    if(imc < 18.6){
-      _infoText = "Abaixo do Peso ( ${imc.toStringAsPrecision(4)} )";
-    }
+    setState((){
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight/(height * height);
+      if(imc < 18.6){
+        _infoText = "Abaixo do Peso ( ${imc.toStringAsPrecision(4)} )";
+      } else if(imc >= 18.6 && imc < 24.9){
+        _infoText = "Peso ideal ( ${imc.toStringAsPrecision(4)} )";
+      }else if(imc >= 24.9 && imc < 29.9){
+        _infoText = "Levemente Acima do Peso ( ${imc.toStringAsPrecision(4)} )";
+      }else if(imc >= 29.9 && imc < 34.9){
+        _infoText = "Obesidade Grau 1 ( ${imc.toStringAsPrecision(4)} )";
+      } else if(imc >= 34.9 && imc < 39.9){
+        _infoText = "Obesidade Grau 2( ${imc.toStringAsPrecision(4)} )";
+      }
+      else if(imc >= 40 ){
+        _infoText = "Obesidade Grau 3 ( ${imc.toStringAsPrecision(4)} )";
+      }
+    });
   }
 
   @override
@@ -54,13 +71,15 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Column(
+        child: Form(
+          key: _formKey,
+          child: Column(
           //alinhar para ocupar toda tela 
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Icon(Icons.person_outline, size:120.0, color: Colors.green),
             //Peso
-            TextField(keyboardType:  TextInputType.number,
+            TextFormField(keyboardType:  TextInputType.number,
               decoration: InputDecoration(
                 labelText: "Peso(Kg)",
                 labelStyle: TextStyle(color: Colors.green)
@@ -68,23 +87,37 @@ class _HomeState extends State<Home> {
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
               controller: weightController,
+              validator: (value){
+                if(value.isEmpty){
+                  return "Insira seu Peso!";
+                }
+              },
             ),
             //Altura
-            TextField(keyboardType:  TextInputType.number,
+            TextFormField(keyboardType:  TextInputType.number,
               decoration: InputDecoration(
                 labelText: "Altura (cm)",
                 labelStyle: TextStyle(color: Colors.green)
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
-              controller: heightController
+              controller: heightController,
+              validator: (value){
+                if(value.isEmpty){
+                  return "Insira seu Altura!";
+                }
+              },
             ),
             Padding(
               padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
               child: Container(
                 height: 50.0,
                 child: RaisedButton(
-                  onPressed: calculate,
+                  onPressed: (){
+                    if(_formKey.currentState.validate()){
+                      calculate();
+                    }
+                  },
                   child: Text("Calcular", 
                     style: TextStyle(color: Colors.white, fontSize: 25.0)
                   ),
@@ -99,6 +132,7 @@ class _HomeState extends State<Home> {
             )
           ],
         ),
+        )
       )
     );
   }
